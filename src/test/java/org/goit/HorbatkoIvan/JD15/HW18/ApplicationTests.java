3f5527075250504010e996c0b8f061f62b3e3198
Ivan Horbatko <44212827+Horbatko-Ivan@users.noreply.github.com>
2024-04-06 01:24:37 +0200

    Java CI-test

--- src/test/java/org/goit/HorbatkoIvan/JD15/HW18/ApplicationTests.java
+++ src/test/java/org/goit/HorbatkoIvan/JD15/HW18/ApplicationTests.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(NoteController.class)
@@ -27,6 +28,7 @@ class NoteControllerTests {
   private NoteService noteService;
 
   @Test
+  @WithMockUser(username = "user", roles = {"USER"})
   void testListNotesForUser() throws Exception {
     Note note = new Note();
     note.setId(1L);
@@ -44,6 +46,7 @@ class NoteControllerTests {
   }
 
   @Test
+  @WithMockUser(username = "user", roles = {"USER"})
   void testGetNoteByIdForUser() throws Exception {
     Note note = new Note();
     note.setId(1L);
